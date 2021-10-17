8b3adb0dcb09e287635e54987f23ea6f8d7fc2f9
Anuj Agrawal <68493064+AnujAgrawal-1@users.noreply.github.com>
2021-10-17 18:52:11 +0530

    Update Main.java

--- Chapter01/P02_FirstNonRepeatedCharacter/src/modern/challenge/Main.java
+++ Chapter01/P02_FirstNonRepeatedCharacter/src/modern/challenge/Main.java
@@ -3,7 +3,7 @@ package modern.challenge;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-
+//haa
     private static final String TEXT = "My high school, the Illinois Mathematics and Science Academy, "
             + "showed me that anything is possible and that you're never too young to think big. "
             + "At 15, I worked as a computer programmer at the Fermi National Accelerator Laboratory, "
